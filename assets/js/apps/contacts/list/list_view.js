e7debfd42d49f3c627cacb60f2be40ee26a70f9a
David Sulc <davidsulc@gmail.com>
2013-05-17 23:08:25 +0200

    removing a contact from the collection

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -4,11 +4,23 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
     template: "#contact-list-item",
 
     events: {
-      "click": "highlightName"
+      "click": "highlightName",
+      "click button.js-delete": "deleteClicked"
     },
 
     highlightName: function(e){
       this.$el.toggleClass('warning');
+    },
+
+    deleteClicked: function(e){
+      e.stopPropagation();
+      this.trigger("contact:delete", this.model);
+    },
+
+    remove: function(){
+      this.$el.fadeOut(function(){
+        $(this).remove();
+      });
     }
   });
 

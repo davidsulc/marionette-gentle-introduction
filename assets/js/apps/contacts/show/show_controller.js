4c7f1642f04ab3735d474150d23eda8350f8830c
David Sulc <davidsulc@gmail.com>
2013-05-22 19:03:28 +0200

    implementing a View for Nonexistent Contacts

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -3,9 +3,15 @@ ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbon
     showContact: function(id){
       var contacts = ContactManager.request("contact:entities");
       var model = contacts.get(id);
-      var contactView = new Show.Contact({
-        model: model
-      });
+      var contactView;
+      if(model !== undefined){
+        contactView = new Show.Contact({
+          model: model
+        });
+      }
+      else{
+        contactView = new Show.MissingContact();
+      }
 
       ContactManager.mainRegion.show(contactView);
     }

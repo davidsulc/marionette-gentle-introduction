c3e5e1ed183239d166f225a41cbf0130fb4fcdc6
David Sulc <davidsulc@gmail.com>
2013-05-22 19:03:28 +0200

    implementing a View for Nonexistent Contacts

--- assets/js/apps/contacts/show/show_view.js
+++ assets/js/apps/contacts/show/show_view.js
@@ -1,4 +1,8 @@
 ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbone, Marionette, $, _){
+  Show.MissingContact = Marionette.ItemView.extend({
+    template: "#missing-contact-view"
+  });
+
   Show.Contact = Marionette.ItemView.extend({
     template: "#contact-view"
   });

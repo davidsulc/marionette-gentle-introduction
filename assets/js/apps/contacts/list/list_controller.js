f1e7f23ec9d9b49ba1dd23e176e9055a9342f9ca
David Sulc <davidsulc@gmail.com>
2013-05-25 17:59:03 +0200

    loading view for contacts list

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -1,6 +1,9 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Controller = {
     listContacts: function(){
+      var loadingView = new ContactManager.Common.Views.Loading();
+      ContactManager.mainRegion.show(loadingView);
+
       var fetchingContacts = ContactManager.request("contact:entities");
 
       $.when(fetchingContacts).done(function(contacts){

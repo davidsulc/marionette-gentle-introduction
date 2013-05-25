b03bf41b32d103c7743aae465b44e32c57561e0a
David Sulc <davidsulc@gmail.com>
2013-05-25 17:00:02 +0200

    add loading view to sub-module

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -1,6 +1,9 @@
 ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbone, Marionette, $, _){
   Show.Controller = {
     showContact: function(id){
+      var loadingView = new ContactManager.Common.Views.Loading();
+      ContactManager.mainRegion.show(loadingView);
+
       var fetchingContact = ContactManager.request("contact:entity", id);
       $.when(fetchingContact).done(function(contact){
         var contactView;
---
+++ assets/js/common/views.js
@@ -0,0 +1,27 @@
+ContactManager.module("Common.Views", function(Views, ContactManager, Backbone, Marionette, $, _){
+  Views.Loading = Marionette.ItemView.extend({
+    template: "#loading-view",
+
+    onShow: function(){
+      var opts = {
+        lines: 13, // The number of lines to draw
+        length: 20, // The length of each line
+        width: 10, // The line thickness
+        radius: 30, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: "#000", // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: "spinner", // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: "30px", // Top position relative to parent in px
+        left: "auto" // Left position relative to parent in px
+      };
+      $("#spinner").spin(opts);
+    }
+  });
+});

ee904942f3d5bccaf5f7cad48371031bb0c41ad3
David Sulc <davidsulc@gmail.com>
2013-05-26 21:22:42 +0200

    data validation and error display when editing a contact

--- assets/js/apps/contacts/edit/edit_view.js
+++ assets/js/apps/contacts/edit/edit_view.js
@@ -8,7 +8,31 @@ ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbon
 
     submitClicked: function(e){
       e.preventDefault();
-      console.log("edit contact");
+      var data = Backbone.Syphon.serialize(this);
+      this.trigger("form:submit", data);
+    },
+
+    onFormDataInvalid: function(errors){
+      var $view = this.$el;
+
+      var clearFormErrors = function(){
+        var $form = $view.find("form");
+        $form.find(".help-inline.error").each(function(){
+          $(this).remove();
+        });
+        $form.find(".control-group.error").each(function(){
+          $(this).removeClass("error");
+        });
+      }
+
+      var markErrors = function(value, key){
+        var $controlGroup = $view.find("#contact-" + key).parent();
+        var $errorEl = $("<span>", { class: "help-inline error", text: value });
+        $controlGroup.append($errorEl).addClass("error");
+      }
+
+      clearFormErrors();
+      _.each(errors, markErrors);
     }
   });
 });
--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,6 +1,24 @@
 ContactManager.module("Entities", function(Entities, ContactManager, Backbone, Marionette, $, _){
   Entities.Contact = Backbone.Model.extend({
-    urlRoot: "contacts"
+    urlRoot: "contacts",
+
+    validate: function(attrs, options) {
+      var errors = {}
+      if (! attrs.firstName) {
+        errors.firstName = "can't be blank";
+      }
+      if (! attrs.lastName) {
+        errors.lastName = "can't be blank";
+      }
+      else{
+        if (attrs.lastName.length < 2) {
+          errors.lastName = "is too short";
+        }
+      }
+      if( ! _.isEmpty(errors)){
+        return errors;
+      }
+    }
   });
 
   Entities.configureStorage(Entities.Contact);

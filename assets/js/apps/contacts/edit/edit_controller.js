8792f28c375a5d8289179d048a531033ec8923b7
David Sulc <davidsulc@gmail.com>
2013-05-26 21:22:42 +0200

    data validation and error display when editing a contact

--- assets/js/apps/contacts/edit/edit_controller.js
+++ assets/js/apps/contacts/edit/edit_controller.js
@@ -14,6 +14,15 @@ ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbon
           view = new Edit.Contact({
             model: contact
           });
+
+          view.on("form:submit", function(data){
+            if(contact.save(data)){
+              ContactManager.trigger("contact:show", contact.get("id"));
+            }
+            else{
+              view.triggerMethod("form:data:invalid", contact.validationError);
+            }
+          });
         }
         else{
           view = new ContactManager.ContactsApp.Show.MissingContact();

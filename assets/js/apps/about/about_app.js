5fdcefe23caf4fdc4fe9807f1b7643ddb5269c7b
David Sulc <davidsulc@gmail.com>
2013-06-03 19:42:57 +0200

    implementing the About sub-application

---
+++ assets/js/apps/about/about_app.js
@@ -0,0 +1,24 @@
+ContactManager.module('AboutApp', function(AboutApp, ContactManager, Backbone, Marionette, $, _){
+  AboutApp.Router = Marionette.AppRouter.extend({
+    appRoutes: {
+      "about" : "showAbout"
+    }
+  });
+
+  var API = {
+    showAbout: function(){
+      AboutApp.Show.Controller.showAbout();
+    }
+  };
+
+  ContactManager.on("about:show", function(){
+    ContactManager.navigate("about");
+    API.showAbout();
+  });
+
+  ContactManager.addInitializer(function(){
+    new AboutApp.Router({
+      controller: API
+    });
+  });
+});

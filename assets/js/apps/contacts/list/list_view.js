595e7eddfa8f3046b2b1fde5c6da154aac94734a
David Sulc <davidsulc@gmail.com>
2014-06-28 19:11:25 +0200

    display contacts list using a CompositeView

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,11 +1,14 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
-    tagName: "li",
+    tagName: "tr",
     template: "#contact-list-item"
   });
 
-  List.Contacts = Marionette.CollectionView.extend({
-    tagName: "ul",
-    childView: List.Contact
+  List.Contacts = Marionette.CompositeView.extend({
+    tagName: "table",
+    className: "table table-hover",
+    template: "#contact-list",
+    childView: List.Contact,
+    childViewContainer: "tbody"
   });
 });

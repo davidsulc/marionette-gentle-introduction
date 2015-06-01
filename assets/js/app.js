simple-git-user/js_basic_marionette

editing a contact from a modal window

assets/js/app.js
@@ -1,7 +1,8 @@
 var ContactManager = new Marionette.Application();
 
 ContactManager.addRegions({
-  mainRegion: "#main-region"
+  mainRegion: "#main-region",
+  dialogRegion: "#dialog-region"
 });
 
 ContactManager.navigate = function(route,  options){

davidsulc/marionette-gentle-introduction

assets/js/app.js
@@ -14,7 +14,8 @@ ContactManager.on("before:start", function(){
     el: "#app-container",
 
     regions: {
-      main: "#main-region"
+      main: "#main-region",
+      dialog: "#dialog-region"
     }
   });
 